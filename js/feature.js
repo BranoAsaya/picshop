11a4d6a8d53d5964eea4100a27f296a7fa61f08a
Brano <asbrano4@gmail.com>
2024-03-31 00:46:53 +0300

    v.1.1

--- js/feature.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById("menuNav")
-const menuBtn = document.getElementById("menu-btn")
-
-menuBtn.addEventListener("click", () => {
-    menu.classList.toggle("menu-open");
-})
